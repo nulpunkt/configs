6cebdf729bc9979b16fe18ab24815ca49027a983
Jesper <nulpunkt@gmail.com>
2013-05-28 20:39:49 +0200

    added bundle install, for dat extra lazyness

--- vim/vimsetup.sh
+++ vim/vimsetup.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 curl -Sso ~/.vim/autoload/pathogen.vim https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+vim +"BundleInstall" +"qa"
