9ba95019d32914715a8386005d75fbd5b3b1ba0a
Jesper Skovgaard Nielsen <nulpunkt@gmail.com>
2016-01-05 11:32:07 +0100

    Ultisnips needs ftdetect stuff

--- vim/vimsetup.sh
+++ vim/vimsetup.sh
@@ -2,3 +2,4 @@
 curl -Sso ~/.vim/autoload/pathogen.vim https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 vim +"BundleInstall" +"qa"
+ln -s ~/.vim/ultisnips_rep/ftdetect/* ~/.vim/ftdetect/
